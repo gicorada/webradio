863bc375c8563cb1a1171bd0e9f4db76a81e6426
Giacomo Radaelli <gradaellig@protonmail.com>
2023-02-28 20:48:46 +0100

    Modificato file in srs_run
    Dovrebbe coprire tutti i casi particolari.

---
+++ srs_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Codice di Giacomo Radaelli
+
+INSTALLDIR="srs"
+
+if [ -d $INSTALLDIR ]; then
+    echo "La cartella $INSTALLDIR esiste. Si presuppone che il server sia già installato e funzionante"
+    echo "Se vuoi ricostruire il server cancella la cartella srs_server, o apri il file e copia la parte tra ***"
+    
+    cd $INSTALLDIR
+    cd srs/trunk
+    ./objs/srs -c conf/srs.conf
+    echo STATO DI SRS:
+    ./etc/init.d/srs status
+else
+    echo "Sembra che srs non sia ancora stato installato. Installare?"
+    read -r -s -p $'Premi Enter per installare\n' -d $'\n'
+    # Download e costruzione
+    # ***
+    mkdir $INSTALLDIR && cd $INSTALLDIR
+    echo "CLONANDO SRS (PESO STIMATO 250MB, PROCEDURA LENTA)"
+    git clone -b develop https://github.com/ossrs/srs.git
+    cd srs/trunk
+    ./configure
+    make
+    # ***
+    
+    echo "Riesegui questo programma per avviare il server"
+fi
+
+
