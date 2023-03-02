f7d631dd0df4a573bc102770c03c8fbc51ed0541
Giacomo Radaelli <gradaellig@protonmail.com>
2023-03-02 20:51:06 +0100

    Modificata cartella e uscita con cd non valido
    Modificata la cartella di installazione da "srs" a "srs_installation"
    per evitare confusione tra repo clonato e installazione.
    Aggiunto "|| exit" dopo ogni cd, come suggerito dalle estensioni che si
    occupano di correggere il codice

--- srs_run.sh
+++ srs_run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Codice di Giacomo Radaelli
 
-INSTALLDIR="srs"
+INSTALLDIR="srs_installation"
+cd ~ || exit
 
 if [ -d $INSTALLDIR ]; then
     echo "La cartella $INSTALLDIR esiste. Si presuppone che il server sia già installato e funzionante"
     echo "Se vuoi ricostruire il server cancella la cartella srs_server, o apri il file e copia la parte tra ***"
     
-    cd $INSTALLDIR
-    cd srs/trunk
+    cd $INSTALLDIR || exit
+    cd srs/trunk || exit
     ./objs/srs -c conf/srs.conf
     echo STATO DI SRS:
     ./etc/init.d/srs status
@@ -17,10 +18,10 @@ else
     read -r -s -p $'Premi Enter per installare\n' -d $'\n'
     # Download e costruzione
     # ***
-    mkdir $INSTALLDIR && cd $INSTALLDIR
+    mkdir $INSTALLDIR && cd $INSTALLDIR || exit
     echo "CLONANDO SRS (PESO STIMATO 250MB, PROCEDURA LENTA)"
     git clone -b develop https://github.com/ossrs/srs.git
-    cd srs/trunk
+    cd srs/trunk || exit
     ./configure
     make
     # ***
